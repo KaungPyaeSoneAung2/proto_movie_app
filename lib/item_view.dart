496b0efd55fcdc2700568197b3a9ba6106b6449d
KaungPyaeSoneAung2 <kaungpyae.kpp16@gmail.com>
2023-06-17 00:04:18 +0630

    First HW Commit

--- lib/item_view.dart
+++ lib/item_view.dart
@@ -1,47 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:proto_movie_app/constants/dimens.dart';
-import 'package:proto_movie_app/constants/item_lists.dart';
-import 'package:proto_movie_app/widgets/carousel.dart';
-import 'package:proto_movie_app/widgets/category_listview.dart';
-import 'package:proto_movie_app/widgets/movie_listview.dart';
-import 'package:proto_movie_app/widgets/search_box.dart';
-import 'package:proto_movie_app/widgets/search_button.dart';
 
 class ItemView extends StatelessWidget {
   const ItemView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 185, 182, 182),
-        title: Text("Adventru"),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(kSmallPadding),
-        child: Wrap(
-            spacing: kWarpSpacing,
-            runSpacing: kWarpSpacing,
-            children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  SearchBox(),SearchButton()
-                ],
-              ),
-              const CategoryListView(),
-              Carousel(movieList: movieList,playButtonPresent: true),
-              const Text("You May Like", style: TextStyle(fontSize: kBigFontSize, color: Colors.white, fontWeight: FontWeight.bold),),
-              MovieListView( movieList: movieList),
-              const Text("Popular", style: TextStyle(fontSize: kBigFontSize, color: Colors.white, fontWeight: FontWeight.bold),),
-              MovieListView( movieList: movieList),
-              Carousel(movieList: movieList, playButtonPresent: false, )
-              ]
-          ),
-      ),
-      )
-    ;
+    return Container();
   }
 }
